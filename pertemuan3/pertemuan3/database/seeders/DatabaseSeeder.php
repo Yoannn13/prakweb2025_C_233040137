c7d179f5fd7c9f400569d416d41a197adcdb40ff
Yoannn13 <yoanpelalana13@gmail.com>
2025-12-04 16:36:28 +0700

    megupload tugas

--- pertemuan3/pertemuan3/database/seeders/DatabaseSeeder.php
+++ pertemuan3/pertemuan3/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +17,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Create 5 Users
+        for ($i = 1; $i <= 5; $i++) {
+            User::create([
+                'name' => 'User ' . $i,
+                'username' => 'user' . $i,
+                'email' => 'user' . $i . '@example.com',
+                'password' => bcrypt('password'),
+            ]);
+        }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Create 2 Categories
+        Category::create(['name' => 'Technology']);
+        Category::create(['name' => 'Lifestyle']);
+
+        // Create 10 Posts
+        Post::factory(10)->create();
     }
 }
